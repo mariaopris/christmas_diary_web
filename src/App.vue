987efd90cbc5eefc408d2d14cb0e494ab8ad44e8
anamaria.hurduc <anamaria.hurduc@webland.ro>
2024-01-10 16:40:54 +0200

    -initial good commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import "tailwindcss/tailwind.css"
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/" class="underline text-xl">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
