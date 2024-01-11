75a5137c0587d991ec0e19f23f91634565b01d06
ioana <curcaioana26@gmail.com>
2024-01-11 22:43:38 +0200

    add group

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,10 +11,10 @@
         </RouterLink>
       </div>
       <div class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-        <template v-for="(group, group_index) in 6">
+        <template v-for="(group, group_index) in groups">
           <div class="border border-1 p-5 rounded-lg flex-col items-center justify-center hover:bg-gray-100">
-            <p class="flex mb-1 items-center justify-center">Group 1</p>
-            <img class="h-24 w-fit rounded-md" src="/src/assets/generic-photo.jpg" alt="">
+            <p class="flex mb-1 items-center justify-center">{{group.name}}</p>
+<!--            <img class="h-24 w-fit rounded-md" src="/src/assets/generic-photo.jpg" alt="">-->
           </div>
         </template>
       </div>
@@ -25,12 +25,24 @@
 </template>
 
 <script setup lang="ts">
-import {onMounted} from "vue";
+import {onMounted, ref} from "vue";
 import {useUserStore} from "/stores/user-store";
+import axios from "axios";
 
 const userStore = useUserStore();
+const groups = ref([]);
 
-onMounted(() => {
-  console.log('user iddd', userStore.userId)
+const getGroups = async() => {
+  await axios.get('http://127.0.0.1:8000/api/getGroups')
+      .then((response) => {
+        if(response.data.status === 'success'){
+          groups.value = response.data.data;
+        }
+      })
+}
+
+onMounted(async() => {
+  console.log('user iddd', userStore.userId);
+  await getGroups();
 })
 </script>
