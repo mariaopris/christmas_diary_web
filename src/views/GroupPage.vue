01e4af271702a08ca97ca894309717619c84e5dd
Lenovo <anamaria.hurduc@yahoo.com>
2024-01-11 21:41:38 +0200

    add group id

--- src/views/GroupPage.vue
+++ src/views/GroupPage.vue
@@ -1,18 +1,57 @@
 <template>
-    <div>
-        Viewww grouuup
-    </div>
+    <template v-if="isLoading">
+        Is loading...
+    </template>
+    <template v-else>
+        <div class="px-20 py-5">
+            <form>
+                <div class="space-y-12">
+                    <h2 class="text-3xl justify-center flex font-semibold text-gray-900">{{groupName}}</h2>
+                </div>
+                <div>
+
+                </div>
+                <div class="mt-6 flex items-center justify-center gap-x-6">
+                    <RouterLink to="/" class="text-sm font-semibold leading-6 text-gray-900">Cancel</RouterLink>
+                    <button type="submit" class="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500">Save</button>
+                </div>
+            </form>
+        </div>
+    </template>
 </template>
+
 
 <script setup lang="ts">
 import {onMounted, ref} from "vue";
-import {useUserStore} from "/stores/user-store";
+import {useRoute} from "vue-router";
 import axios from "axios";
-import router from "@/router";
 
-const userStore = useUserStore();
+const isLoading = ref(true);
+const groupId = ref('');
+const route = useRoute();
+const groupName = ref('');
 
-onMounted(() => {
-    console.log('user iddd', userStore.userId)
+groupId.value = String(route.params.group_id);
+
+const getGroups = async () => {
+    axios.get('http://127.0.0.1:8000/api/getGroups')
+        .then(response => {
+            console.log(response.data.data);
+            response.data.data.forEach((group) => {
+                if(group.id == groupId.value){
+                    groupName.value = group.name;
+                }
+            })
+
+            isLoading.value = false;
+
+        })
+        .catch(error => {
+
+        });
+}
+
+onMounted(async () => {
+    await getGroups();
 })
 </script>
