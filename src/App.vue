3ac1bd7f40e9c1400281860bf19243cb63dcd30d
Maria Opris <maria.opris@webland.ro>
2024-01-11 19:18:35 +0200

    auth

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <header class="">
       <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
         <div class="flex lg:flex-1">
-          <template v-if="!is_logged_in">
+          <template v-if="auth.getUser === ''">
             <div class="flex items-center space-x-3">
               <img class="h-10 w-10" src="/src/assets/christmas-market.svg" alt="">
               <h class="text-xl text-red-500 font-sans font-bold italic">X-Max Diary</h>
@@ -18,15 +18,15 @@
             </RouterLink>
           </template>
         </div>
-        <div v-if="!is_logged_in" class="hidden lg:flex lg:flex-1 lg:justify-end">
+        <div v-if="auth.getUser !== ''" class="hidden lg:flex lg:flex-1 lg:justify-end">
+          <button @click="logout()" class="text-sm font-semibold leading-6 text-gray-900 p-2" to="/login">Log out <span
+              aria-hidden="true">&rarr;</span></button>
+        </div>
+        <div v-else class="hidden lg:flex lg:flex-1 lg:justify-end">
           <RouterLink class="text-sm font-bold text-red-500 p-2 bg-white" to="/register">Sign up
           </RouterLink>
           <RouterLink class="text-sm font-bold leading-6 text-white p-2 rounded-lg bg-red-500" to="/login">Log in <span
               aria-hidden="true">&rarr;</span></RouterLink>
-        </div>
-        <div v-if="is_logged_in" class="hidden lg:flex lg:flex-1 lg:justify-end">
-          <button @click="logout()" class="text-sm font-semibold leading-6 text-gray-900 p-2" to="/login">Log out <span
-              aria-hidden="true">&rarr;</span></button>
         </div>
       </nav>
 
@@ -38,23 +38,83 @@
 </template>
 
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import {RouterLink, RouterView, useRoute, useRouter} from 'vue-router'
 import {onMounted, ref} from "vue";
 import {useUserStore} from "/stores/user-store";
-import router from "@/router";
+import {useAuthStore} from "../stores/auth";
+import {useCookies} from "vue3-cookies";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const userStore = useUserStore();
 const is_logged_in = ref(false);
+const auth = useAuthStore();
+const {cookies} = useCookies();
+const router = useRouter();
+const route = useRoute();
+const userId = ref(0);
 
-const logout = () => {
-  userStore.setUserId('');
-  is_logged_in.value = false;
-  router.push('/login');
+const logout = async () => {
+  try {
+    await axios.post("http://127.0.0.1:8000/api/logout")
+        .then(async (response) => {
+
+          delete axios.defaults.headers.common.Authorization;
+          cookies.remove('Token', response.data.token);
+          auth.logout();
+
+          await router.push('/login');
+
+        });
+  } catch (e) {
+    console.log(e);
+    if (e.response?.data?.error) {
+      await Swal.fire({
+        icon: 'error',
+        title: 'Ooops..',
+        text: e.response.data.error,
+      })
+    } else {
+      await Swal.fire({
+        icon: 'error',
+        title: 'Ooops..',
+        text: "An unknown error has occured",
+      })
+    }
+  }
 }
 
 onMounted(() => {
   if(userStore.userId !== ''){
     is_logged_in.value = true;
   }
+})
+onMounted(async () => {
+  axios.interceptors.response.use((response) => {
+    return response
+  }, function (error) {
+    return Promise.reject(error)
+  })
+
+  let my_cookie_value = cookies.get("Token");
+
+  axios.defaults.headers.common.Authorization = "Bearer " + my_cookie_value;
+
+  await axios.get("http://127.0.0.1:8000/api/getUser")
+      .then((result) => {
+        console.log('result', result)
+        auth.setUser(result.data);
+        userId.value = result.data.id;
+      })
+      .catch((error) => {
+        console.log('eroare: ', error);
+        if (route.name) {
+          if (route.name !== 'login' && route.name !== 'register') {
+            if (error.response?.status === 401) {
+              router.push('/login');
+            }
+          }
+        }
+      })
 })
 </script>
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="flex justify-center mt-14">
+    <div class="w-full max-w-[500px]">
+      <form class="bg-white shadow-lg rounded px-8  pb-8 mb-4">
+        <p class="block font-bold pt-8 text-3xl text-gray-900 flex justify-center">X-Max Diary</p>
+        <div class="mb-10 mt-5">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+            Email
+          </label>
+          <input v-model="email" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="email" placeholder="Email">
+        </div>
+        <div class="mb-6">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+            Password
+          </label>
+          <input v-model="password" required class="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+        </div>
+        <p class="flex justify-center mt-3 mb-7 text-sm">If you don't have an account <RouterLink to="/register" class="underline font-semibold text-blue-500 ml-2"> Sign Up</RouterLink></p>
+
+        <div class="flex items-center justify-center">
+          <button class="bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" @click="login()">
+            Log In
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+import axios from "axios";
+import Swal from "sweetalert2";
+import {useCookies} from "vue3-cookies";
+import {useAuthStore} from "/stores/auth";
+import {useRouter} from "vue-router";
+
+const email = ref('');
+const password = ref('');
+
+const auth = useAuthStore();
+const {cookies} = useCookies();
+const router = useRouter();
+
+const login = async () =>{
+  await axios.post("http://127.0.0.1:8000/api/login", {
+    email: email.value,
+    password: password.value,
+  })
+      .then(async (result) => {
+        axios.defaults.headers.common.Authorization = "Bearer " + result.data.token;
+        cookies.set("Token", result.data.token, '5h');
+        auth.setUser(result.data);
+        await router.push('/');
+      }).catch((e) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'An error has occurred!',
+          text: e,
+        })
+      });
+}
+</script>
